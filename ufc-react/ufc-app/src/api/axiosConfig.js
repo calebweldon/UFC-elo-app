82f89133aa97b7162779688b1a0492fd1db0f5a2
calebweldon <caleb.weldon@atscale.com>
2025-07-22 18:34:46 -0700

    STYLE: make fighter table responsive

--- ufc-react/ufc-app/src/api/axiosConfig.js
+++ ufc-react/ufc-app/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://ufc-springboot-297559815689.us-west1.run.app",
     headers: {"ngrok-skip-browser-warning": "true"}
 });
