c3858189ed2f3f747eca121fd2e33192465b7575
calebweldon <caleb.weldon@atscale.com>
2025-07-22 21:09:39 -0700

    UPDATE: fix link

--- ufc-react/ufc-app/src/api/axiosConfig.js
+++ ufc-react/ufc-app/src/api/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    // baseURL: "https://ufc-springboot-297559815689.us-west1.run.app",
-    baseURL: "http://localhost:8080",
+    baseURL: "https://ufc-springboot-297559815689.us-west1.run.app",
     headers: {"ngrok-skip-browser-warning": "true"}
 });
